49d782689b0263a5bcf5a3f595fbb36c1ce48b85
marizmelo <mm@xchema.com>
2016-11-23 12:14:56 -0800

    pakage.json, webpack config, test disrupt file

---
+++ src/disrupt.js
@@ -0,0 +1,8 @@
+// Author: Mariz Melo
+// license: http://rem.mit-license.org
+// source: https://github.com/marizmelo/disrupt
+
+var Disrupt = (function (){
+  'use strict';
+  console.log('Hello World');
+})(this);
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  entry: './src/disrupt.js',
+  output: {
+    filename: 'disrupt.js',
+    path: './dist'
+  }
+}
